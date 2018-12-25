a2697b997249afc8023f363f99c4561ab3fb39ed
Neil Nie <yongyang.nie@gmail.com>
2018-12-25 18:21:12 -0500

    tensorboard log added
    and cleaned up train.py
    other small changes.

--- train.py
+++ train.py
@@ -6,48 +6,43 @@
 
 
 import models.enet_naive_upsampling.model as enet
+from keras.callbacks import TensorBoard
+import matplotlib.pyplot as plt
 import configs
 import utils
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas
 import os
+import datetime
 
 
 def train(model, epochs, steps_epch, train_gen, save_path, test_result=False):
 
-    model.fit_generator(generator=train_gen, steps_per_epoch=steps_epch, epochs=epochs, verbose=1)
-    model.save(save_path)
+    tensorboard = TensorBoard(log_dir=("logs/" + save_path + "/{}".format(datetime.datetime.now())))
+
+    model.fit_generator(generator=train_gen, steps_per_epoch=steps_epch, epochs=epochs, verbose=1, callbacks=[tensorboard])
+    model.save(save_path + ".h5")
 
     if test_result:
 
         # Plotting generator output
         images, targets = next(train_generator)
 
-        for i in range(4):
-            im = np.array(images[i], dtype=np.uint8)
-            im_mask = np.array(targets[i], dtype=np.uint8)
-            img = np.array([im], dtype=np.uint8)
-            im_prediction = model.predict(img)[0]
+        for i in range(len(images)):
+            im_gt = np.array(targets[i], dtype=np.uint8)
+            im_prediction = model.predict(np.array(images[i], dtype=np.uint8))[0]
             plt.subplot(1, 3, 1)
-            plt.imshow(im)
-            plt.axis('off')
+            plt.imshow(np.array(images[i], dtype=np.uint8))
             plt.subplot(1, 3, 2)
-            plt.imshow(im_mask[:, :, 0])
-            plt.axis('off')
+            plt.imshow(im_gt[:, :, 0])
             plt.subplot(1, 3, 3)
             plt.imshow(im_prediction[:, :, 0])
-            plt.axis('off')
             plt.show()
 
 
-if __name__ == "__main__":
+def load_data():
 
-    model = enet.build(len(utils.labels), configs.img_height, configs.img_width)
-    print(model.summary())
-
-    label_path = configs.data_path
-    labels = pandas.read_csv(label_path).values
+    labels = pandas.read_csv(configs.data_path).values
     df = []
     count = 0
     for row in labels:
@@ -58,15 +53,17 @@ if __name__ == "__main__":
     print("data processing finished")
     print("data frame size: " + str(count))
 
-    train_generator = utils.train_generator(df, 1)
+    return df
 
-    inputs, outputs = next(train_generator)
 
-    for i in range(len(utils.labels)):
-        plt.imshow(outputs[0, :, :, i], cmap='gray')
-        plt.show()
+if __name__ == "__main__":
 
-    exit(0)
+    model = enet.build(len(utils.labels), configs.img_height, configs.img_width)
+    print(model.summary())
 
-    # model.load_weights("./enet_v3.h5")
-    train(model, epochs=10, train_gen=train_generator, steps_epch=1000, save_path="./enet_v1.h5")
+    df = load_data()
+
+    train_generator = utils.train_generator(df, 4)
+
+    model.load_weights("./enet_v3.h5")
+    train(model, epochs=5, train_gen=train_generator, steps_epch=200, save_path="./enet_v4")
